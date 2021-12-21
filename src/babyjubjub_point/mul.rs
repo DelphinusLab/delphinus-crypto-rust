89c9ad95db2811c1efdf60cf8dbffb595cfd14a6
suzuharahato <499067887@qq.com>
2021-12-22 00:12:11 +0800

    perf: pre compute

--- src/babyjubjub_point/mul.rs
+++ src/babyjubjub_point/mul.rs
@@ -1,6 +1,6 @@
 use super::*;
 use sp_std::ops::Mul;
-use num_bigint::{BigInt};
+use num_bigint::{BigInt, Sign};
 use num_traits::{Zero};
 use std::vec::Vec;
 
@@ -11,6 +11,15 @@ impl<'a, 'b> Mul<&'b BigInt> for &'a BabyJubjubPoint {
         let mut acc = BabyJubjubPoint::get_origin().clone();
         let mut k = other.clone();
         let mut vec = Vec::new();
+
+        let mut pre = Vec::new();
+        pre.push(self.clone());
+        let double = self + self;
+        let mut i = 1;
+        while i < 8 {
+            pre.push(&pre[i-1] + &double);
+            i += 1;
+        }
 
         while !k.is_zero() {
             if k.is_odd() {
@@ -26,7 +35,8 @@ impl<'a, 'b> Mul<&'b BigInt> for &'a BabyJubjubPoint {
         while let Some(d) = vec.pop() {
             acc = &acc + &acc;
             if d.is_odd() {
-                acc = &acc + self._mul(&d);
+                let (_, ds) = d.to_u32_digits();
+                acc = &acc + &pre[(ds[0] >> 1) as usize]
             }
         }
 
