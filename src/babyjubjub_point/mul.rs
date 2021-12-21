073c8dc904b0148426e910418a1aa68b7dbc2418
suzuharahato <499067887@qq.com>
2021-12-21 23:01:06 +0800

    perf: wnaf method for point mul

--- src/babyjubjub_point/mul.rs
+++ src/babyjubjub_point/mul.rs
@@ -1,12 +1,36 @@
 use super::*;
 use sp_std::ops::Mul;
 use num_bigint::{BigInt};
+use num_traits::{Zero};
+use std::vec::Vec;
 
 impl<'a, 'b> Mul<&'b BigInt> for &'a BabyJubjubPoint {
     type Output = BabyJubjubPoint;
 
     fn mul(self, other: &BigInt) -> BabyJubjubPoint {
-        self._mul(other)
+        let mut acc = BabyJubjubPoint::get_origin().clone();
+        let mut k = other.clone();
+        let mut vec = Vec::new();
+
+        while !k.is_zero() {
+            if k.is_odd() {
+                let d: BigInt = &k % 16;
+                k = &k - &d;
+                vec.push(d);
+            } else {
+                vec.push(BigInt::zero())
+            }
+            k = k >> 1u32;
+        }
+
+        while let Some(d) = vec.pop() {
+            acc = &acc + &acc;
+            if d.is_odd() {
+                acc = &acc + self._mul(&d);
+            }
+        }
+
+        acc
     }
 }
 
