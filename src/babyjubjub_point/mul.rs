8cfc9e5e68069d04311619e2bcda655960d51a85
suzuharahato <499067887@qq.com>
2021-12-22 20:06:51 +0800

    refactor

--- src/babyjubjub_point/mul.rs
+++ src/babyjubjub_point/mul.rs
@@ -1,6 +1,6 @@
 use super::*;
 use sp_std::ops::Mul;
-use num_bigint::{BigInt, Sign};
+use num_bigint::{BigInt};
 use num_traits::{Zero};
 use std::vec::Vec;
 
@@ -8,31 +8,35 @@ impl<'a, 'b> Mul<&'b BigInt> for &'a BabyJubjubPoint {
     type Output = BabyJubjubPoint;
 
     fn mul(self, other: &BigInt) -> BabyJubjubPoint {
+        // ref: https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication#w-ary_non-adjacent_form_(wNAF)_method
         let mut acc = BabyJubjubPoint::get_origin().clone();
         let mut k = other.clone();
-        let mut vec = Vec::new();
+        let w = 1 << 4;
 
-        let mut pre = Vec::new();
-        pre.push(self.clone());
-        let double = self + self;
-        let mut i = 1;
-        while i < 8 {
-            pre.push(&pre[i-1] + &double);
-            i += 1;
-        }
-
+        // Compute the non-adjacent form of the multiplicand
+        let mut naf = Vec::new();
         while !k.is_zero() {
             if k.is_odd() {
-                let d: BigInt = &k % 16;
+                let d: BigInt = &k % w;
                 k = &k - &d;
-                vec.push(d);
+                naf.push(d);
             } else {
-                vec.push(BigInt::zero())
+                naf.push(BigInt::zero())
             }
             k = k >> 1u32;
         }
 
-        while let Some(d) = vec.pop() {
+        // Pre compute {1,3,5,...,w-1}P
+        let mut pre = Vec::new();
+        pre.push(self.clone());
+        let double = self + self;
+        let mut i = 1;
+        while i < (w >> 1) {
+            pre.push(&pre[i-1] + &double);
+            i += 1;
+        }
+
+        while let Some(d) = naf.pop() {
             acc = &acc + &acc;
             if d.is_odd() {
                 let (_, ds) = d.to_u32_digits();
